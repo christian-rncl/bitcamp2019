7da83a9e18c43575e4b536af9074e505767e95b4
Christian Roncal <cjl.roncal@gmail.com>
2019-04-13 14:23:07 -0400

    moved folders around

---
+++ data.py
@@ -0,0 +1,9 @@
+'''
+Author(s): Christian Roncal
+'''
+import torch
+import pandas as pd
+import numpy as np
+import numpy.random as rand
+from torch.utils.data import DataLoader, Dataset, TensorDataset
+from sklearn.model_selection import train_test_split
