dd5e8b99adee7384f623ed5cd503184c13aff2e2
Shane Miller <shanekmiller44@gmail.com>
2019-04-13 11:06:01 -0400

    Disabled Duplication

--- mmpeg_test/ffmpeg_test.py
+++ mmpeg_test/ffmpeg_test.py
@@ -10,21 +10,12 @@ def createPath(s):
     except OSError:  
         assert False, "Creation of the directory %s failed."
 
-def deletePath(s):
-    try:  
-        rmtree(s,ignore_errors=False)
-    except OSError:  
-        print ("Deletion of the directory %s failed" % s)
-        print(OSError)
-
-
 
 for filename in os.listdir('test_data'):
     path_name = filename + '_frames'
-    if os.path.exists(path_name):
-        deletePath(path_name)
+    
+    if not os.path.exists(path_name):
+        createPath(path_name)
 
-    createPath(path_name)
-
-    command = 'ffmpeg -i ' + i + ' -qscale:v 2 ' + path_name + '/frame%03d.jpg'
-    subprocess.call(command, shell=True)
+        command = 'ffmpeg -i ' + i + ' -qscale:v 2 ' + path_name + '/frame%03d.jpg'
+        subprocess.call(command, shell=True)
