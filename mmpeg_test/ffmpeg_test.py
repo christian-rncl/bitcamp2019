3d8c20a1dbeb09e3708dc5a014b548216496082d
Shane Miller <shanekmiller44@gmail.com>
2019-04-13 10:14:52 -0400

    Upgraded!

--- mmpeg_test/ffmpeg_test.py
+++ mmpeg_test/ffmpeg_test.py
@@ -1,6 +1,28 @@
 import subprocess
+import os
 
+def createPath(s):
+    #assert (not os.path.exists(s)), "The filepath "+s+" already exists. Don't want to overwrite it. Aborting."
+
+    try:  
+        os.mkdir(s)
+    except OSError:  
+        assert False, "Creation of the directory %s failed."
+
+def deletePath(s):
+    try:  
+        rmtree(s,ignore_errors=False)
+    except OSError:  
+        print ("Deletion of the directory %s failed" % s)
+        print(OSError)
+
+path_name = 'frames'
 input_file = 'cat.mp4'
 
-command = 'ffmpeg -i ' + input_file + ' -qscale:v 2 frame%06d.jpg'
+if os.path.exists(path_name):
+    deletePath(path_name)
+
+createPath(path_name)
+
+command = 'ffmpeg -i ' + input_file + ' -qscale:v 2 frames/frame%03d.jpg'
 subprocess.call(command, shell=True)
