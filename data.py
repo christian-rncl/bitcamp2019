1168573e820106edbacb738bb22ac6366e4a5c06
Christian Roncal <cjl.roncal@gmail.com>
2019-04-13 17:22:03 -0400

    playing with lang model

--- data.py
+++ data.py
@@ -4,3 +4,8 @@ import numpy as np
 import numpy.random as rand
 from torch.utils.data import DataLoader, Dataset, TensorDataset
 from sklearn.model_selection import train_test_split
+
+
+class Generator:
+
+    def __init__
