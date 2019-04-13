1c6e77261c9a63235172feb99ce6f6a32ae4e655
christian-rncl <cjl.roncal@gmail.com>
2019-04-13 19:35:15 -0400

    ffmpeg test files

--- model.py
+++ model.py
@@ -2,13 +2,19 @@ import torch.nn as nn
 import torch.nn.init as init
 
 def getModel(n_in, enc_h, rnn_h, n_rnnlayers, n_signs, dropout=0.2):
+    print(rnn_h)
     net = nn.Sequential(
         nn.Linear(n_in, enc_h),
         nn.BatchNorm1d(enc_h),
         nn.ReLU(),
         nn.Dropout(dropout),
-        nn.GRU(enc_h, rnn_h, n_rnnlayers, n_signs, nn)
+        nn.GRU(enc_h, rnn_h, n_rnnlayers, dropout=dropout),
+        nn.Linear(rnn_h, n_signs),
+        nn.BatchNorm1d(rnn_h),
+        nn.Sigmoid()
     )
+
+    return net
 
 # class SignToText(nn.Module):
 
