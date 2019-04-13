85489a0b8320d20bac8b29282e9a79ac302d3de9
Shane Miller <shanekmiller44@gmail.com>
2019-04-13 15:41:50 -0400

    Fixed FFMPEG Script to Fit Needs

--- ffmpeg_test/ffmpeg_test.py
+++ ffmpeg_test/ffmpeg_test.py
@@ -1,21 +1,53 @@
 import subprocess
 import os
-from shutil import rmtree
 
 def createPath(s):
-    #assert (not os.path.exists(s)), "The filepath "+s+" already exists. Don't want to overwrite it. Aborting."
-
     try:  
         os.mkdir(s)
     except OSError:  
         assert False, "Creation of the directory %s failed."
 
-
-for filename in os.listdir('test_data'):
-    path_name = 'test_data_frames/' + filename + '_frames'
+for filename in os.listdir('test_data/on'):
+    path_name = 'test_data_frames/on/' + filename
 
     if not os.path.exists(path_name):
         createPath(path_name)
 
-        command = 'ffmpeg -i test_data/' + filename + ' -qscale:v 2 ' + path_name + '/frame%03d.jpg'
+        command = 'ffmpeg -i test_data/on/' + filename + ' -qscale:v 2 ' + path_name + '/%04d.jpg'
+        subprocess.call(command, shell=True)
+
+for filename in os.listdir('test_data/off'):
+    path_name = 'test_data_frames/off/' + filename
+
+    if not os.path.exists(path_name):
+        createPath(path_name)
+
+        command = 'ffmpeg -i test_data/off/' + filename + ' -qscale:v 2 ' + path_name + '/%04d.jpg'
+        subprocess.call(command, shell=True)
+
+for filename in os.listdir('test_data/up'):
+    path_name = 'test_data_frames/up/' + filename
+
+    if not os.path.exists(path_name):
+        createPath(path_name)
+
+        command = 'ffmpeg -i test_data/up/' + filename + ' -qscale:v 2 ' + path_name + '/%04d.jpg'
+        subprocess.call(command, shell=True)
+
+for filename in os.listdir('test_data/down'):
+    path_name = 'test_data_frames/down/' + filename
+
+    if not os.path.exists(path_name):
+        createPath(path_name)
+
+        command = 'ffmpeg -i test_data/down/' + filename + ' -qscale:v 2 ' + path_name + '/%04d.jpg'
+        subprocess.call(command, shell=True)
+
+for filename in os.listdir('test_data/music_on'):
+    path_name = 'test_data_frames/music_on/' + filename
+
+    if not os.path.exists(path_name):
+        createPath(path_name)
+
+        command = 'ffmpeg -i test_data/music_on/' + filename + ' -qscale:v 2 ' + path_name + '/%04d.jpg'
         subprocess.call(command, shell=True)
