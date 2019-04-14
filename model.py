cd7077bb72e197e82da27dc2f95eb76837040b38
christian-rncl <cjl.roncal@gmail.com>
2019-04-13 20:51:15 -0400

    asd

--- model.py
+++ model.py
@@ -1,30 +1,39 @@
 import torch.nn as nn
 import torch.nn.init as init
 
-def getModel(n_in, enc_h, rnn_h, n_rnnlayers, n_signs, dropout=0.2):
-    print(rnn_h)
-    net = nn.Sequential(
-        nn.Linear(n_in, enc_h),
-        nn.BatchNorm1d(enc_h),
-        nn.ReLU(),
-        nn.Dropout(dropout),
-        nn.GRU(enc_h, rnn_h, n_rnnlayers, dropout=dropout),
-        nn.Linear(rnn_h, n_signs),
-        nn.BatchNorm1d(rnn_h),
-        nn.Sigmoid()
-    )
+# def getModel(n_in, enc_h, rnn_h, n_rnnlayers, n_signs, dropout=0.2):
+    # print(rnn_h)
+    # net = nn.Sequential(
+    #     nn.Linear(n_in, enc_h),
+    #     # nn.BatchNorm1d(num_features=enc_h),
+    #     nn.ReLU(),
+    #     nn.Dropout(dropout),
+    #     nn.GRU(enc_h, rnn_h),
+    #     nn.Linear(rnn_h, n_signs),
+    #     # nn.BatchNorm1d(rnn_h),
+    #     nn.Sigmoid()
+    # )
 
-    return net
+    # return net
 
-# class SignToText(nn.Module):
+class SignToText(nn.Module):
 
-#     def __init__(self, dropout=0.2, n_input, n_hidden, n_signs, n_layers, afn):
-#         self.encoder = nn.Linear(n_in, enc_dim)
-#         self.rnn = nn.GRU(enc_dim, n_hidden, n_layers, nonlinearity=afn, dropout=dropout)
-#         self.rnn_bn = nn.BatchNorm2d(n_hidden)
-#         self.decoder = nn.Linear(n_hidden, n_signs)
-#         self.sigmoid = nn.Sigmoid()
-#         self.init_weights()
+    def __init__(self, n_in, enc_h, rnn_h, n_rnnlayers, n_signs, dropout=0.2):
+        super(SignToText, self).__init__()
 
-#     def init_weights(self):
-#         init.xavier_normal(self.rnn)
+        self.encoder = nn.Linear(n_in, enc_h)
+        self.enc_bn = nn.BatchNorm1d(enc_h)
+        self.dropout = nn.Dropout(dropout)
+        self.rnn = nn.GRU(enc_h, rnn_h)
+        self.rnn_bn = nn.BatchNorm1d(rnn_h)
+        self.decoder = nn.Linear(rnn_h, n_signs)
+        self.sigmoid = nn.Sigmoid()
+        self.init_weights()
+
+    def init_weights(self):
+        init.xavier_normal(self.encoder)
+        init.xavier_normal(self.rnn)
+        init.xavier_normal(self.rnn_bn)
+        init.xavier_normal(self.decoder)
+
+    def forward()
